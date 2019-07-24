3d31fd2364afd2f3a0e3ab582ebd265f3b7afdf6
Samantha Breaux <Sam.fbreaux@gmail.com>
2019-07-24 01:11:31 -0700

    x

--- drugplate2.R
+++ drugplate2.R
@@ -12,7 +12,7 @@ library(scales)
 ?mtpview1
 ##load data proccessed
 
-GD<-read.csv("2019-07-02_summarised-data (1).csv")
+GD<-read.csv("2019-07-24_summarised-data.csv")
 
 ## merge PD with Plate Map
 
@@ -26,29 +26,29 @@ data <- add_plate(
 x<-split(data, data$plate, drop = F)
 
 BY4743<-x$S1L1
-U5.05<-x$S1L2
-S6.14<-x$S2L1
-S6.45<-x$S2L2
-S6.113<-x$S3L1
-W.34.70<-x$S3L2
-WLP644<-x$S4L1
 
+U5.05<-x$S2L1
+S6.14<-x$S2L2
+S6.45<-x$S3L1
+s6.113<-x$S3L2
+W.34.70<-x$S4L1
+WLP644<-x$S4L2
 
 #load in raw data
 
-MD<-read.csv("2019-07-02_measures-data (2).csv")
+MD<-read.csv("2019-07-24_measures-data (1).csv")
 
 ##split dataframe based on plate 
 xx<-split(MD, MD$plate, drop = F)
 
 
 BY4743m<-xx$S1L1
-U5.05m<-xx$S1L2
-S6.14m<-xx$S2L1
-S6.45m<-xx$S2L2
-S6.113m<-xx$S3L1
-W.34.70m<-xx$S3L2
-WLP644m<-xx$S4L1
+U5.05m<-xx$S2L1
+S6.14m<-xx$S2L2
+S6.45m<-xx$S3L1
+s6.113m<-xx$S3L2
+W.34.70m<-xx$S4L1
+WLP644m<-xx$S4L2
 
 
 ## function to calculate area undercurve
@@ -73,137 +73,137 @@ calc_AUC <- function(n,z){
 ## function to calculate area undercurve of a plate and return it as a dataframe
 
 calc_AUC_plate <- function(BY4743m){
-
-A01<- calc_AUC(BY4743m,"A01") 
-B01<- calc_AUC(BY4743m,"B01") 
-C01<- calc_AUC(BY4743m,"C01") 
-D01<- calc_AUC(BY4743m,"D01") 
-E01<- calc_AUC(BY4743m,"E01") 
-F01<- calc_AUC(BY4743m,"F01") 
-G01<- calc_AUC(BY4743m,"G01")  
-H01<- calc_AUC(BY4743m,"H01")  
-
-A02<- calc_AUC(BY4743m,"A02") 
-B02<- calc_AUC(BY4743m,"B02") 
-C02<- calc_AUC(BY4743m,"C02")  
-D02<- calc_AUC(BY4743m,"D02") 
-E02<- calc_AUC(BY4743m,"E02") 
-F02<- calc_AUC(BY4743m,"F02") 
-G02<- calc_AUC(BY4743m,"G02") 
-H02<- calc_AUC(BY4743m,"H02") 
-
-A03<- calc_AUC(BY4743m,"A03") 
-B03<- calc_AUC(BY4743m,"B03") 
-C03<- calc_AUC(BY4743m,"C03") 
-D03<- calc_AUC(BY4743m,"D03") 
-E03<- calc_AUC(BY4743m,"E03") 
-F03<- calc_AUC(BY4743m,"F03") 
-G03<- calc_AUC(BY4743m,"G03") 
-H03<- calc_AUC(BY4743m,"H03") 
-
-A04<- calc_AUC(BY4743m,"A04") 
-B04<- calc_AUC(BY4743m,"B04") 
-C04<- calc_AUC(BY4743m,"C04") 
-D04<- calc_AUC(BY4743m,"D04") 
-E04<- calc_AUC(BY4743m,"E04") 
-F04<- calc_AUC(BY4743m,"F04") 
-G04<- calc_AUC(BY4743m,"G04") 
-H04<- calc_AUC(BY4743m,"H04") 
-
-A05<- calc_AUC(BY4743m,"A05") 
-B05<- calc_AUC(BY4743m,"B05") 
-C05<- calc_AUC(BY4743m,"C05") 
-D05<- calc_AUC(BY4743m,"D05") 
-E05<- calc_AUC(BY4743m,"E05") 
-F05<- calc_AUC(BY4743m,"F05") 
-G05<- calc_AUC(BY4743m,"G05") 
-H05<- calc_AUC(BY4743m,"H05") 
-
-A06<- calc_AUC(BY4743m,"A06") 
-B06<- calc_AUC(BY4743m,"B06") 
-C06<- calc_AUC(BY4743m,"C06") 
-D06<- calc_AUC(BY4743m,"D06") 
-E06<- calc_AUC(BY4743m,"E06") 
-F06<- calc_AUC(BY4743m,"F06") 
-G06<- calc_AUC(BY4743m,"G06") 
-H06<- calc_AUC(BY4743m,"H06") 
-
-A07<- calc_AUC(BY4743m,"A07") 
-B07<- calc_AUC(BY4743m,"B07") 
-C07<- calc_AUC(BY4743m,"C07") 
-D07<- calc_AUC(BY4743m,"D07") 
-E07<- calc_AUC(BY4743m,"E07") 
-F07<- calc_AUC(BY4743m,"F07") 
-G07<- calc_AUC(BY4743m,"G07") 
-H07<- calc_AUC(BY4743m,"H07")
-
-A08<- calc_AUC(BY4743m,"A08") 
-B08<- calc_AUC(BY4743m,"B08") 
-C08<- calc_AUC(BY4743m,"C08") 
-D08<- calc_AUC(BY4743m,"D08") 
-E08<- calc_AUC(BY4743m,"E08") 
-F08<- calc_AUC(BY4743m,"F08") 
-G08<- calc_AUC(BY4743m,"G08") 
-H08<- calc_AUC(BY4743m,"H08") 
-
-A09<- calc_AUC(BY4743m,"A09") 
-B09<- calc_AUC(BY4743m,"B09") 
-C09<- calc_AUC(BY4743m,"C09") 
-D09<- calc_AUC(BY4743m,"D09") 
-E09<- calc_AUC(BY4743m,"E09")
-F09<- calc_AUC(BY4743m,"F09")
-G09<- calc_AUC(BY4743m,"G09")
-H09<- calc_AUC(BY4743m,"H09")
-
-A10<- calc_AUC(BY4743m,"A10") 
-B10<- calc_AUC(BY4743m,"B10") 
-C10<- calc_AUC(BY4743m,"C10") 
-D10<- calc_AUC(BY4743m,"D10") 
-E10<- calc_AUC(BY4743m,"E10")
-F10<- calc_AUC(BY4743m,"F10")
-G10<- calc_AUC(BY4743m,"G10")
-H10<- calc_AUC(BY4743m,"H10")
-
-A11<- calc_AUC(BY4743m,"A11") 
-B11<- calc_AUC(BY4743m,"A11") 
-B11<- calc_AUC(BY4743m,"B11") 
-C11<- calc_AUC(BY4743m,"C11") 
-D11<- calc_AUC(BY4743m,"D11") 
-E11<- calc_AUC(BY4743m,"E11")
-F11<- calc_AUC(BY4743m,"F11")
-G11<- calc_AUC(BY4743m,"G11")
-H11<- calc_AUC(BY4743m,"H11")
-
-A12<- calc_AUC(BY4743m,"A12") 
-B12<- calc_AUC(BY4743m,"B12") 
-C12<- calc_AUC(BY4743m,"C12") 
-D12<- calc_AUC(BY4743m,"D12") 
-E12<- calc_AUC(BY4743m,"E12")
-F12<- calc_AUC(BY4743m,"F12")
-G12<- calc_AUC(BY4743m,"G12")
-H12<- calc_AUC(BY4743m,"H12")
-
-
-AUC<-tibble(A01,	B01,	C01,	D01,	E01,	F01,	G01,	H01, 
-            A02,	B02,	C02,	D02,	E02,	F02,	G02,	H02,
-            A03,	B03,	C03,	D03,	E03,	F03,	G03,	H03,
-            A04,	B04,	C04,	D04,	E04,	F04,	G04,	H04,
-            A05,	B05,	C05,	D05,	E05,	F05,	G05,	H05,
-            A06,	B06,	C06,	D06,	E06,	F06,	G06,	H06,
-            A07,	B07,	C07,	D07,	E07,	F07,	G07,	H07,
-            A08,	B08,	C08,	D08,	E08,	F08,	G08,	H08,
-            A09,	B09,	C09,	D09,	E09,	F09,	G09,	H09,
-            A10,	B10,	C10,	D10,	E10,	F10,	G10,	H10,
-            A11,	B11,	C11,	D11,	E11,	F11,	G11,	H11,
-            A12,	B12,	C12,	D12,	E12,	F12,	G12,	H12)
-
-auc<-melt(AUC)
-names(auc) <- c("well", "auc")
-aucdata <- add_plate(
-  data = auc, 
-  file = "PLATE MAP.csv",
-  well_ids_column = "well")
-
+  
+  A01<- calc_AUC(BY4743m,"A01") 
+  B01<- calc_AUC(BY4743m,"B01") 
+  C01<- calc_AUC(BY4743m,"C01") 
+  D01<- calc_AUC(BY4743m,"D01") 
+  E01<- calc_AUC(BY4743m,"E01") 
+  F01<- calc_AUC(BY4743m,"F01") 
+  G01<- calc_AUC(BY4743m,"G01")  
+  H01<- calc_AUC(BY4743m,"H01")  
+  
+  A02<- calc_AUC(BY4743m,"A02") 
+  B02<- calc_AUC(BY4743m,"B02") 
+  C02<- calc_AUC(BY4743m,"C02")  
+  D02<- calc_AUC(BY4743m,"D02") 
+  E02<- calc_AUC(BY4743m,"E02") 
+  F02<- calc_AUC(BY4743m,"F02") 
+  G02<- calc_AUC(BY4743m,"G02") 
+  H02<- calc_AUC(BY4743m,"H02") 
+  
+  A03<- calc_AUC(BY4743m,"A03") 
+  B03<- calc_AUC(BY4743m,"B03") 
+  C03<- calc_AUC(BY4743m,"C03") 
+  D03<- calc_AUC(BY4743m,"D03") 
+  E03<- calc_AUC(BY4743m,"E03") 
+  F03<- calc_AUC(BY4743m,"F03") 
+  G03<- calc_AUC(BY4743m,"G03") 
+  H03<- calc_AUC(BY4743m,"H03") 
+  
+  A04<- calc_AUC(BY4743m,"A04") 
+  B04<- calc_AUC(BY4743m,"B04") 
+  C04<- calc_AUC(BY4743m,"C04") 
+  D04<- calc_AUC(BY4743m,"D04") 
+  E04<- calc_AUC(BY4743m,"E04") 
+  F04<- calc_AUC(BY4743m,"F04") 
+  G04<- calc_AUC(BY4743m,"G04") 
+  H04<- calc_AUC(BY4743m,"H04") 
+  
+  A05<- calc_AUC(BY4743m,"A05") 
+  B05<- calc_AUC(BY4743m,"B05") 
+  C05<- calc_AUC(BY4743m,"C05") 
+  D05<- calc_AUC(BY4743m,"D05") 
+  E05<- calc_AUC(BY4743m,"E05") 
+  F05<- calc_AUC(BY4743m,"F05") 
+  G05<- calc_AUC(BY4743m,"G05") 
+  H05<- calc_AUC(BY4743m,"H05") 
+  
+  A06<- calc_AUC(BY4743m,"A06") 
+  B06<- calc_AUC(BY4743m,"B06") 
+  C06<- calc_AUC(BY4743m,"C06") 
+  D06<- calc_AUC(BY4743m,"D06") 
+  E06<- calc_AUC(BY4743m,"E06") 
+  F06<- calc_AUC(BY4743m,"F06") 
+  G06<- calc_AUC(BY4743m,"G06") 
+  H06<- calc_AUC(BY4743m,"H06") 
+  
+  A07<- calc_AUC(BY4743m,"A07") 
+  B07<- calc_AUC(BY4743m,"B07") 
+  C07<- calc_AUC(BY4743m,"C07") 
+  D07<- calc_AUC(BY4743m,"D07") 
+  E07<- calc_AUC(BY4743m,"E07") 
+  F07<- calc_AUC(BY4743m,"F07") 
+  G07<- calc_AUC(BY4743m,"G07") 
+  H07<- calc_AUC(BY4743m,"H07")
+  
+  A08<- calc_AUC(BY4743m,"A08") 
+  B08<- calc_AUC(BY4743m,"B08") 
+  C08<- calc_AUC(BY4743m,"C08") 
+  D08<- calc_AUC(BY4743m,"D08") 
+  E08<- calc_AUC(BY4743m,"E08") 
+  F08<- calc_AUC(BY4743m,"F08") 
+  G08<- calc_AUC(BY4743m,"G08") 
+  H08<- calc_AUC(BY4743m,"H08") 
+  
+  A09<- calc_AUC(BY4743m,"A09") 
+  B09<- calc_AUC(BY4743m,"B09") 
+  C09<- calc_AUC(BY4743m,"C09") 
+  D09<- calc_AUC(BY4743m,"D09") 
+  E09<- calc_AUC(BY4743m,"E09")
+  F09<- calc_AUC(BY4743m,"F09")
+  G09<- calc_AUC(BY4743m,"G09")
+  H09<- calc_AUC(BY4743m,"H09")
+  
+  A10<- calc_AUC(BY4743m,"A10") 
+  B10<- calc_AUC(BY4743m,"B10") 
+  C10<- calc_AUC(BY4743m,"C10") 
+  D10<- calc_AUC(BY4743m,"D10") 
+  E10<- calc_AUC(BY4743m,"E10")
+  F10<- calc_AUC(BY4743m,"F10")
+  G10<- calc_AUC(BY4743m,"G10")
+  H10<- calc_AUC(BY4743m,"H10")
+  
+  A11<- calc_AUC(BY4743m,"A11") 
+  B11<- calc_AUC(BY4743m,"A11") 
+  B11<- calc_AUC(BY4743m,"B11") 
+  C11<- calc_AUC(BY4743m,"C11") 
+  D11<- calc_AUC(BY4743m,"D11") 
+  E11<- calc_AUC(BY4743m,"E11")
+  F11<- calc_AUC(BY4743m,"F11")
+  G11<- calc_AUC(BY4743m,"G11")
+  H11<- calc_AUC(BY4743m,"H11")
+  
+  A12<- calc_AUC(BY4743m,"A12") 
+  B12<- calc_AUC(BY4743m,"B12") 
+  C12<- calc_AUC(BY4743m,"C12") 
+  D12<- calc_AUC(BY4743m,"D12") 
+  E12<- calc_AUC(BY4743m,"E12")
+  F12<- calc_AUC(BY4743m,"F12")
+  G12<- calc_AUC(BY4743m,"G12")
+  H12<- calc_AUC(BY4743m,"H12")
+  
+  
+  AUC<-tibble(A01,	B01,	C01,	D01,	E01,	F01,	G01,	H01, 
+              A02,	B02,	C02,	D02,	E02,	F02,	G02,	H02,
+              A03,	B03,	C03,	D03,	E03,	F03,	G03,	H03,
+              A04,	B04,	C04,	D04,	E04,	F04,	G04,	H04,
+              A05,	B05,	C05,	D05,	E05,	F05,	G05,	H05,
+              A06,	B06,	C06,	D06,	E06,	F06,	G06,	H06,
+              A07,	B07,	C07,	D07,	E07,	F07,	G07,	H07,
+              A08,	B08,	C08,	D08,	E08,	F08,	G08,	H08,
+              A09,	B09,	C09,	D09,	E09,	F09,	G09,	H09,
+              A10,	B10,	C10,	D10,	E10,	F10,	G10,	H10,
+              A11,	B11,	C11,	D11,	E11,	F11,	G11,	H11,
+              A12,	B12,	C12,	D12,	E12,	F12,	G12,	H12)
+  
+  auc<-melt(AUC)
+  names(auc) <- c("well", "auc")
+  aucdata <- add_plate(
+    data = auc, 
+    file = "PLATE MAP.csv",
+    well_ids_column = "well")
+  
 }
 
 
@@ -381,14 +381,14 @@ write.csv(WLPstats, "WLPstats.csv")
 
 
 g<-rbind(BYstats,
-U5stats,
-S14stats,
-S45stats,
-S113stats,
-W34stats,
-WLPstats)
+         U5stats,
+         S14stats,
+         S45stats,
+         S113stats,
+         W34stats,
+         WLPstats)
 
-  
+
 write.csv(BYaverage_stats, "BYaverage_stats.csv")
 write.csv(U5average_stats, "U5average_stats.csv")
 write.csv(S14average_stats, "S14average_stats.csv")
@@ -416,6 +416,13 @@ W34stats<-merge(BY4743stats, U5stats)%>%
   mutate(BYrel_auc= auc/BYauc, BYrel_mu= mu/BYmu, con_rel_auc = auc/76260.33, con_rel_mu = mu/1.9e-05)
 WLPstats<-merge(BY4743stats, U5stats)%>%
   mutate(BYrel_auc= auc/BYauc, BYrel_mu= mu/BYmu, con_rel_auc = auc/108883.04, con_rel_mu = mu/6.6e-05)
+
+
+BYstats[ , "BYmu"] <- NA
+BYstats[ , "BYauc"] <- NA
+BYstats[ , "BYrel_mu"] <- NA
+BYstats[ , "BYrel_auc"] <- NA
+
 
 
 plot_plate_mu<-function(BY4743m, BYstats){
@@ -514,11 +521,5 @@ plot_plate_conmu<-function(BY4743m, BYstats){
 }
 
 
-audit::run()
 
-names(BYstats)
-names(U5stats)
-BYstats[ , "BYmu"] <- NA
-BYstats[ , "BYauc"] <- NA
-BYstats[ , "BYrel_mu"] <- NA
-BYstats[ , "BYrel_auc"] <- NA
+
